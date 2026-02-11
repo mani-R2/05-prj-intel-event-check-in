1d9519655809435e0f279472b5eb75ac0b6ae6e5
imani rojas <iyrojas@asu.edu>
2026-02-11 23:09:18 +0000

    added DOM elements; function to handle form submissions

---
+++ script.js
@@ -0,0 +1,11 @@
+// Get all neeeded DOM elements
+const form = document.getElementById("checkInForm");
+const nameInput = document.getElementById("attendeeName");
+const teamSelect = document.getElementById("teamSelect");
+
+// Function to handle form submission
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+})
+
+  // Get the values from the form
